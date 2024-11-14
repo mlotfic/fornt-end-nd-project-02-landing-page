75fa5945082e02aa4aa164bf48e1aef8c82e1f86
mahmoud lotfi <59091299+mlotfic@users.noreply.github.com>
2024-11-15 00:29:21 +0200

    feat: JS Navbar highlighted feature

--- js/app.js
+++ js/app.js
@@ -30,22 +30,54 @@ console.log(sections);
 const navList  = document.getElementById("navbar__list");
 console.log(navList);
 
+
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-
+/**
+* @description Function to check which section is currently in view and Add active state class to section in navbar when it is near top of viewport
+* @param {number} sections
+* @param {number} navLinks
+*/
+// 
+function makeActive(sections, navLinks) {
+    sections.forEach((section, index) => {
+      const box = section.getBoundingClientRect();    
+      // Check if the section is in the viewport
+      if (box.top <= window.innerHeight / 2 && box.bottom >= window.innerHeight / 2) {
+        /* apply active state on the current section and the corresponding Nav link.
+           by removing active class from all links */
+        console.log(index);
+        
+        navLinks.forEach(link => {
+            console.log(link);
+            // Ensure navLinks[index] exists before accessing classList
+            link.classList.remove("active");    
+        });
+        /* remove active state from other section and corresponding Nav link.
+           by add active class to the current section's link
+        */
+       // Ensure navLinks[index] exists before accessing classList
+       if (navLinks[index]) {
+        navLinks[index].classList.add("active");
+        console.log(navLinks[index].classList);
+        console.log(index);
+        }
+      }
+    });
+  }
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
-// build the nav
-sections.forEach(section => {     
+sections.forEach(section => {  
+    /* ------------------------- build the nav ------------------------ */   
     /* create Anchor element.
        setting attribute:
        - textContent:section-data-nav
@@ -62,14 +94,29 @@ sections.forEach(section => {
     
     // attach to navbar
     navList.appendChild(navItem);
-    navItem.appendChild(navLink);
+    navItem.appendChild(navLink); 
+
+    /* ---------- Scroll to anchor ID using scrollTO event ----------- */
+    navLink.addEventListener("click", () => {        
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: "smooth"
+          });
+      });
+
   });
 
-// Add class 'active' to section when near top of viewport
 
+/* --- The active section in the Navbar should be highlighted. --- */
 
-// Scroll to anchor ID using scrollTO event
+// Select all anchors inside navigation
+const navLinks = document.querySelectorAll(".menu__link");
+// console.log(navLinks);
 
+// Navbar section viewed active
+document.addEventListener("scroll", () => {
+    makeActive(sections, navLinks);
+});
 
 /**
  * End Main Functions
