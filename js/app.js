356dbe29e4f22c3cb1f2996c8488bc147e5bc601
mahmoud lotfi <59091299+mlotfic@users.noreply.github.com>
2024-11-14 21:20:01 +0200

    feat: "js" build the nav

--- js/app.js
+++ js/app.js
@@ -22,7 +22,13 @@
  * Define Global Variables
  * 
 */
+// Select all sections and the navigation list container
+const sections = document.querySelectorAll("section");
+console.log(sections);
 
+// Select Page navbar empty list by id:`navbar__list`
+const navList  = document.getElementById("navbar__list");
+console.log(navList);
 
 /**
  * End Global Variables
@@ -39,7 +45,25 @@
 */
 
 // build the nav
+sections.forEach(section => {     
+    /* create Anchor element.
+       setting attribute:
+       - textContent:section-data-nav
+       - href: section-id
+       - classlist:"menu__link"    
+    */
+    const navLink = document.createElement("a");  
+    navLink.textContent = section.getAttribute("data-nav");
+    navLink.href = `#${section.id}`;
+    navLink.classList.add("menu__link");
 
+    // create an item list then add to page navbar
+    const navItem = document.createElement("li");
+    
+    // attach to navbar
+    navList.appendChild(navItem);
+    navItem.appendChild(navLink);
+  });
 
 // Add class 'active' to section when near top of viewport
 
